ce356c0b6debc010910ad9e90dbf29c6363eaa74
Starrier <1342878298@qq.com>
2017-12-17 03:39:33 +0800

    BootStrap Time Changed

--- app/src/main/java/org/starrier/coffee/bootstrap/GuidePagerActivity.java
+++ app/src/main/java/org/starrier/coffee/bootstrap/GuidePagerActivity.java
@@ -57,7 +57,7 @@ public class GuidePagerActivity extends FragmentActivity {
         init();
         setPageChange();
         Timer timer = new Timer();
-        new Handler().postDelayed(runnable, 5000);
+        new Handler().postDelayed(runnable, 4300);
 
 
     }
